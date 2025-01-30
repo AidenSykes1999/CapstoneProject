57beec8802a57ef13d33ca673776eb516e743fef
Aiden Sykes <aidensykes1999@gmail.com>
2025-01-30 14:51:08 +0000

    Step 4 initialised

--- foodie-admin/src/app/app-routing.module.ts
+++ foodie-admin/src/app/app-routing.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AddRestaurantComponent } from './add-restaurant/add-restaurant.component';
+import { AddDishComponent } from './add-dish/add-dish.component';
+import { ViewRestaurantComponent } from './view-restaurant/view-restaurant.component';
+import { ViewDishComponent } from './view-dish/view-dish.component';
+import { AuthGuard } from './service/auth.guard';
 
-
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'add-restaurant', component: AddRestaurantComponent, canActivate: [AuthGuard] },
+  { path: 'add-dish', component: AddDishComponent, canActivate: [AuthGuard] },
+  { path: 'view-restaurant', component: ViewRestaurantComponent, canActivate: [AuthGuard] },
+  { path: 'view-dish', component: ViewDishComponent, canActivate: [AuthGuard] },
+  { path: '**', redirectTo: '/login' } // Redirect to login for any unknown route
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- foodie-admin/src/app/login/login.component.ts
+++ foodie-admin/src/app/login/login.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
 
+  username: string = '';
+  password: string = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onSubmit(): void {
+    if (this.authService.login(this.username, this.password)) {
+      this.router.navigate(['/dashboard']);
+    } else {
+      alert('Invalid credentials');
+    }
+  }
 }
